07923cb361d2d6ed2cdf1adf38da0130c6c506d2
philkre <p.kreiter@icloud.com>
2026-06-04 13:51:47 +0200

    fix: remove stale comment from CoopDisaster module

--- sim/src/CoopDisaster.jl
+++ sim/src/CoopDisaster.jl
@@ -4,7 +4,6 @@ module CoopDisaster
     include("types.jl")
     include("lcp.jl")
 
-    # Remaining files added in later tasks
     export PlayerType, UC, CC, FR
     export LcpParams, SimConfig, DEFAULT_CONFIG
     export contribution
